a3516c7a1c682d9ed42f0d972316d98ebb3b30fe
Stephane Bourdeaud <stephane.bourdeaud@nutanix.com>
2021-11-16 18:12:10 +0100

    added VM_LIST as an option to specify a comma separated list of vm names for which to collect perf metrics

--- python/prometheus-exporter/build/nutanix-prometheus-exporter.py
+++ python/prometheus-exporter/build/nutanix-prometheus-exporter.py
@@ -183,21 +183,89 @@ def prism_get_cluster(api_server,username,secret,secure=False):
         ))
         raise
 
+def prism_get_vm(vm_name,api_server,username,secret,secure=False):
+    """Retrieves data from the Prism Element v2 REST API endpoint /vms using a vm name as a filter criteria.
+
+    Args:
+        vm_name: The VM name to search for.
+        api_server: The IP or FQDN of Prism.
+        username: The Prism user name.
+        secret: The Prism user name password.
+        
+    Returns:
+        VM details as vm_details
+    """
+    
+    #region prepare the api call
+    headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+    }
+    api_server_port = int(os.getenv("APP_PORT", "9440"))
+    api_server_endpoint = f"/PrismGateway/services/rest/v1/vms/?filterCriteria=vm_name%3D%3D{vm_name}"
+    url = "https://{}:{}{}".format(
+        api_server,
+        api_server_port,
+        api_server_endpoint
+    )
+    method = "GET"
+    #endregion
+    
+    print("Making a {} API call to {} with secure set to {}".format(method, url, secure))
+    resp = process_request(url,method,username,secret,headers,secure=secure)
+
+    # deal with the result/response
+    if resp.ok:
+        json_resp = json.loads(resp.content)
+        vm_details = json_resp['entities']
+        return vm_details[0]
+    else:
+        print("Request failed!")
+        print("status code: {}".format(resp.status_code))
+        print("reason: {}".format(resp.reason))
+        print("text: {}".format(resp.text))
+        print("raise_for_status: {}".format(resp.raise_for_status()))
+        print("elapsed: {}".format(resp.elapsed))
+        print("headers: {}".format(resp.headers))
+        print("payload: {}".format(payload))
+        print(json.dumps(
+            json.loads(resp.content),
+            indent=4
+        ))
+        raise
+
 class NutanixMetrics:
     """
     Representation of Prometheus metrics and loop to fetch and transform
     application metrics into Prometheus metrics.
     """
     
-    def __init__(self, app_port=9440, polling_interval_seconds=5, prism='127.0.0.1', user='admin', pwd='Nutanix/4u', prism_secure=False):
+    def __init__(self, app_port=9440, polling_interval_seconds=5, prism='127.0.0.1', user='admin', pwd='Nutanix/4u', prism_secure=False, vm_list=''):
         self.app_port = app_port
         self.polling_interval_seconds = polling_interval_seconds
         self.prism = prism
         self.user = user
         self.pwd = pwd
         self.prism_secure = prism_secure
+        self.vm_list = vm_list
         
         cluster_uuid, cluster_details = prism_get_cluster(api_server=prism,username=user,secret=pwd,secure=self.prism_secure)
+        
+        if self.vm_list:
+            vm_list_array = self.vm_list.split(',')
+            vm_details = prism_get_vm(vm_name=vm_list_array[0],api_server=prism,username=user,secret=pwd,secure=self.prism_secure)
+            for key,value in vm_details['stats'].items():
+                #making sure we are compliant with the data model (https://prometheus.io/docs/concepts/data_model/#metric-names-and-labels)
+                key_string = f"nutanix_vms_stats_{key}"
+                key_string = key_string.replace(".","_")
+                key_string = key_string.replace("-","_")
+                setattr(self, key_string, Gauge(key_string, key_string, ['vm']))
+            for key,value in vm_details['usageStats'].items():
+                #making sure we are compliant with the data model (https://prometheus.io/docs/concepts/data_model/#metric-names-and-labels)
+                key_string = f"nutanix_vms_usage_stats_{key}"
+                key_string = key_string.replace(".","_")
+                key_string = key_string.replace("-","_")
+                setattr(self, key_string, Gauge(key_string, key_string, ['vm']))
         
         # Prometheus metrics to collect
         for key,value in cluster_details['stats'].items():
@@ -205,13 +273,13 @@ class NutanixMetrics:
             key_string = f"nutanix_clusters_stats_{key}"
             key_string = key_string.replace(".","_")
             key_string = key_string.replace("-","_")
-            setattr(self, key_string, Gauge(key_string, key_string))
+            setattr(self, key_string, Gauge(key_string, key_string, ['cluster']))
         for key,value in cluster_details['usage_stats'].items():
             #making sure we are compliant with the data model (https://prometheus.io/docs/concepts/data_model/#metric-names-and-labels)
             key_string = f"nutanix_clusters_usage_stats_{key}"
             key_string = key_string.replace(".","_")
             key_string = key_string.replace("-","_")
-            setattr(self, key_string, Gauge(key_string, key_string))
+            setattr(self, key_string, Gauge(key_string, key_string, ['cluster']))
 
     def run_metrics_loop(self):
         """Metrics fetching loop"""
@@ -233,19 +301,36 @@ class NutanixMetrics:
             key_string = f"nutanix_clusters_stats_{key}"
             key_string = key_string.replace(".","_")
             key_string = key_string.replace("-","_")
-            self.__dict__[key_string].set(value)
+            self.__dict__[key_string].labels(cluster=cluster_details['name']).set(value)
         for key, value in cluster_details['usage_stats'].items():
             #making sure we are compliant with the data model (https://prometheus.io/docs/concepts/data_model/#metric-names-and-labels)
             key_string = f"nutanix_clusters_usage_stats_{key}"
             key_string = key_string.replace(".","_")
             key_string = key_string.replace("-","_")
-            self.__dict__[key_string].set(value)
+            self.__dict__[key_string].labels(cluster=cluster_details['name']).set(value)
+        
+        if self.vm_list:
+            vm_list_array = self.vm_list.split(',')
+            for vm in vm_list_array:
+                vm_details = prism_get_vm(vm_name=vm,api_server=self.prism,username=self.user,secret=self.pwd,secure=self.prism_secure)
+                for key, value in vm_details['stats'].items():
+                    #making sure we are compliant with the data model (https://prometheus.io/docs/concepts/data_model/#metric-names-and-labels)
+                    key_string = f"nutanix_vms_stats_{key}"
+                    key_string = key_string.replace(".","_")
+                    key_string = key_string.replace("-","_")
+                    self.__dict__[key_string].labels(vm=vm_details['vmName']).set(value)
+                for key, value in vm_details['usageStats'].items():
+                    #making sure we are compliant with the data model (https://prometheus.io/docs/concepts/data_model/#metric-names-and-labels)
+                    key_string = f"nutanix_vms_usage_stats_{key}"
+                    key_string = key_string.replace(".","_")
+                    key_string = key_string.replace("-","_")
+                    self.__dict__[key_string].labels(vm=vm_details['vmName']).set(value)
         
 
 def main():
     """Main entry point"""
 
-    polling_interval_seconds = int(os.getenv("POLLING_INTERVAL_SECONDS", "5"))
+    polling_interval_seconds = int(os.getenv("POLLING_INTERVAL_SECONDS", "30"))
     app_port = int(os.getenv("APP_PORT", "9440"))
     exporter_port = int(os.getenv("EXPORTER_PORT", "8000"))
 
@@ -255,7 +340,8 @@ def main():
         prism=os.getenv('PRISM'),
         user = os.getenv('PRISM_USERNAME'),
         pwd = os.getenv('PRISM_SECRET'),
-        prism_secure=bool(os.getenv("PRISM_SECURE", False))
+        prism_secure=bool(os.getenv("PRISM_SECURE", False)),
+        vm_list=os.getenv('VM_LIST')
     )
     
     start_http_server(exporter_port)
