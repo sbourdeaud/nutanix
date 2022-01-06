bd3e510315492512aeafe475654a793f54938959
Stephane Bourdeaud <stephane.bourdeaud@nutanix.com>
2022-01-06 10:13:28 +0100

    adding tasks with work in progress for karbon

---
+++ calm/task-library/karbon/KarbonRemoveWorkerNode.py
@@ -0,0 +1,175 @@
+# escript-template v20190611 / stephane.bourdeaud@nutanix.com
+# TODO Fill in this section with your information
+# * author:     stephane.bourdeaud@nutanix.com
+# * version:    v1/20220105
+# task_name:    KarbonRemoveWorkerNode
+# description:  Removes worker node(s) from an existing Karbon managed k8s cluster. Using Karbon API: https://www.nutanix.dev/api_references/karbon/#/ZG9jOjQ1Mg-karbon-api-reference
+# inputvars:    See inputvars region below
+# outputvars:   remove_task_uuid
+
+import requests
+import json
+
+
+#region inputvars
+#* credentials
+pc_user = "@@{prism_central.username}@@"
+pc_password = "@@{prism_central.secret}@@"
+
+#* input variables
+remove_worker_node_count = int("@@{add_worker_node_count}@@")
+cluster_name = "@@{cluster_name}@@"
+node_pool = "@@{node_pool}@@"
+#endregion inputvars
+
+
+#region functions
+
+def process_request(url, method, user, password, headers, payload=None, secure=False):
+    if payload is not None:
+        payload = json.dumps(payload)
+    
+    #configuring web request behavior
+    timeout=10
+    retries = 5
+    sleep_between_retries = 5
+    
+    while retries > 0:
+        try:
+
+            if method == 'POST':
+                    r = requests.post(
+                    url,
+                    headers=headers,
+                    data=payload,
+                    auth=(user, password),
+                    verify=secure,
+                    timeout=timeout
+                )
+            elif method == 'DELETE':
+                r = requests.delete(
+                    url,
+                    headers=headers,
+                    data=payload,
+                    auth=(user, password),
+                    verify=secure,
+                    timeout=timeout
+                )
+            elif method == 'GET':
+                r = requests.get(
+                    url,
+                    headers=headers,
+                    auth=(user, password),
+                    verify=secure,
+                    timeout=timeout
+                )
+            elif method == 'PUT':
+                    r = requests.put(
+                    url,
+                    headers=headers,
+                    data=payload,
+                    auth=(user, password),
+                    verify=secure,
+                    timeout=timeout
+                )
+            elif method == 'PATCH':
+                    r = requests.put(
+                    url,
+                    headers=headers,
+                    data=payload,
+                    auth=(user, password),
+                    verify=secure,
+                    timeout=timeout
+                )
+        except requests.exceptions.HTTPError as e:
+            print ("Http Error!")
+            print("status code: {}".format(r.status_code))
+            print("reason: {}".format(r.reason))
+            print("text: {}".format(r.text))
+            print("elapsed: {}".format(r.elapsed))
+            print("headers: {}".format(r.headers))
+            if payload is not None:
+                print("payload: {}".format(payload))
+            print(json.dumps(
+                json.loads(r.content),
+                indent=4
+            ))
+            exit(r.status_code)
+        except requests.exceptions.ConnectionError as e:
+            print ("Connection Error!")
+            if retries == 1:
+                print('Error: {c}, Message: {m}'.format(c = type(e).__name__, m = str(e)))
+                exit(1)
+            else:
+                print('Error: {c}, Message: {m}'.format(c = type(e).__name__, m = str(e)))
+                sleep(sleep_between_retries)
+                retries -= 1
+                print ("retries left: {}".format(retries))
+                continue
+            print('Error: {c}, Message: {m}'.format(c = type(e).__name__, m = str(e)))
+            exit(1)
+        except requests.exceptions.Timeout as e:
+            print ("Timeout Error!")
+            if retries == 1:
+                raise Exception(e)
+            else:
+                print('Error! Code: {c}, Message: {m}'.format(c = type(e).__name__, m = str(e)))
+                sleep(sleep_between_retries)
+                retries -= 1
+                print ("retries left: {}".format(retries))
+                continue
+        except requests.exceptions.RequestException as e:
+            print ("Error!")
+            exit(r.status_code)
+        break
+    
+    if r.ok:
+        return r
+    if r.status_code == 401:
+        print("status code: {0}".format(r.status_code))
+        print("reason: {0}".format(r.reason))
+        exit(r.status_code)
+    elif r.status_code == 500:
+        print("status code: {0}".format(r.status_code))
+        print("reason: {0}".format(r.reason))
+        print("text: {0}".format(r.text))
+        exit(r.status_code)
+    else:
+        print("Request failed!")
+        print("status code: {0}".format(r.status_code))
+        print("reason: {0}".format(r.reason))
+        print("text: {0}".format(r.text))
+        print("raise_for_status: {0}".format(r.raise_for_status()))
+        print("elapsed: {0}".format(r.elapsed))
+        print("headers: {0}".format(r.headers))
+        if payload is not None:
+            print("payload: {0}".format(payload))
+        print(json.dumps(
+            json.loads(r.content),
+            indent=4
+        ))
+        exit(r.status_code)
+
+#endregion functions
+
+
+#region prepare api call
+headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
+payload = {
+  "count": remove_worker_node_count
+}
+method = 'POST'
+url = "https://localhost:9440/karbon/v1-alpha.1/k8s/clusters/{}/node-pools/{}/remove-nodes".format(
+    cluster_name,
+    node_pool,
+)
+#endregion prepapre api call
+
+
+#region make api call
+resp = process_request(url, method, pc_user, pc_password, headers, payload)
+print ("Creation of task to remove Worker Node was successful", json.dumps(json.loads(resp.content), indent=4))
+remove_task_uuid = resp.json()['task_uuid']
+print ("task_uuid={}".format(remove_task_uuid))
+exit(0)
+#endregion make api call
